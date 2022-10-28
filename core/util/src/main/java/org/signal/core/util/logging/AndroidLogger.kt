7586bc1bb591bdf7ae8eb0f2eb54c0e490ac0b5f
Abandoned Cart <twistedumbrella@gmail.com>
2022-10-28 18:32:16 -0400

    Add a flag to ignore excessive use of verbose log

--- core/util/src/main/java/org/signal/core/util/logging/AndroidLogger.kt
+++ core/util/src/main/java/org/signal/core/util/logging/AndroidLogger.kt
@@ -4,26 +4,29 @@ import android.annotation.SuppressLint
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
+import org.signal.core.util.BuildConfig
 
 @SuppressLint("LogNotSignal")
 object AndroidLogger : Log.Logger() {
 
+  private val isLogVerbose: Boolean get() = BuildConfig.VERBOSE_LOGGING
+
   private val serialExecutor: Executor = Executors.newSingleThreadExecutor { Thread(it, "signal-logcat") }
 
   override fun v(tag: String, message: String?, t: Throwable?, keepLonger: Boolean) {
-    serialExecutor.execute {
+    if (isLogVerbose) serialExecutor.execute {
       android.util.Log.v(tag, message.scrub(), t)
     }
   }
 
   override fun d(tag: String, message: String?, t: Throwable?, keepLonger: Boolean) {
-    serialExecutor.execute {
+    if (isLogVerbose) serialExecutor.execute {
       android.util.Log.d(tag, message.scrub(), t)
     }
   }
 
   override fun i(tag: String, message: String?, t: Throwable?, keepLonger: Boolean) {
-    serialExecutor.execute {
+    if (isLogVerbose) serialExecutor.execute {
       android.util.Log.i(tag, message.scrub(), t)
     }
   }
