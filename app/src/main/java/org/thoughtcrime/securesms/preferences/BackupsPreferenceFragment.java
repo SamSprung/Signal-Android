b983ee0ffbd88c5ffd9e892f217c918cc2f75b9c
Abandoned Cart <twistedumbrella@gmail.com>
2022-10-28 18:33:39 -0400

    Add an option to keep screen on during backups

--- app/src/main/java/org/thoughtcrime/securesms/preferences/BackupsPreferenceFragment.java
+++ app/src/main/java/org/thoughtcrime/securesms/preferences/BackupsPreferenceFragment.java
@@ -11,6 +11,7 @@ import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -67,6 +68,7 @@ public class BackupsPreferenceFragment extends Fragment {
   private TextView    folderName;
   private ProgressBar progress;
   private TextView    progressSummary;
+  private CheckBox    screenCheckBox;
 
   private final NumberFormat formatter = NumberFormat.getInstance();
 
@@ -91,9 +93,11 @@ public class BackupsPreferenceFragment extends Fragment {
     folderName      = view.findViewById(R.id.fragment_backup_folder_name);
     progress        = view.findViewById(R.id.fragment_backup_progress);
     progressSummary = view.findViewById(R.id.fragment_backup_progress_summary);
+    screenCheckBox  = view.findViewById(R.id.fragment_backup_progress_screen_on);
 
     toggle.setOnClickListener(unused -> onToggleClicked());
     create.setOnClickListener(unused -> onCreateClicked());
+    screenCheckBox.setOnCheckedChangeListener((compoundButton, enabled) -> screenCheckBox.setKeepScreenOn(enabled));
     verify.setOnClickListener(unused -> BackupDialog.showVerifyBackupPassphraseDialog(requireContext()));
     timer.setOnClickListener(unused -> pickTime());
 
@@ -151,6 +155,8 @@ public class BackupsPreferenceFragment extends Fragment {
       progress.setVisibility(View.VISIBLE);
       progressSummary.setVisibility(event.getCount() > 0 ? View.VISIBLE : View.GONE);
 
+      screenCheckBox.setVisibility(View.VISIBLE);
+
       if (event.getEstimatedTotalCount() == 0) {
         progress.setIndeterminate(true);
         progressSummary.setText(getString(R.string.BackupsPreferenceFragment__d_so_far, event.getCount()));
@@ -166,6 +172,8 @@ public class BackupsPreferenceFragment extends Fragment {
       create.setEnabled(true);
       progress.setVisibility(View.GONE);
       progressSummary.setVisibility(View.GONE);
+      screenCheckBox.setChecked(false);
+      screenCheckBox.setVisibility(View.GONE);
       setBackupSummary();
       ThreadUtil.runOnMainDelayed(this::setBackupSummary, 100);
     }
