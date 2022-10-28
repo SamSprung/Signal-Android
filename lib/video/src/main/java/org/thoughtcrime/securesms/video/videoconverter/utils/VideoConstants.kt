135cbffc45121d20032265bfb24cafa43c03f157
Abandoned Cart <twistedumbrella@gmail.com>
2022-10-28 18:30:23 -0400

    Reduce media size restrictions and compression

--- lib/video/src/main/java/org/thoughtcrime/securesms/video/videoconverter/utils/VideoConstants.kt
+++ lib/video/src/main/java/org/thoughtcrime/securesms/video/videoconverter/utils/VideoConstants.kt
@@ -12,18 +12,23 @@ import org.thoughtcrime.securesms.video.TranscodingConfig.TranscodeConfig
 object VideoConstants {
   const val KB: Int = 1000
   const val MB: Int = 1000 * KB
-  const val VIDEO_SHORT_EDGE_HD = 720
-  const val VIDEO_LONG_EDGE_HD = 1280
+  const val AUDIO_BITRATE = 192_000
+  const val VIDEO_BITRATE_L1 = 3_500_000
+  const val VIDEO_BITRATE_L2 = 4_500_000
+  const val VIDEO_BITRATE_L3 = 6_000_000
+  const val VIDEO_SHORT_EDGE_SD = 720
+  const val VIDEO_SHORT_EDGE_HD = 1080
+  const val VIDEO_LONG_EDGE_HD = 1920
   const val VIDEO_MAX_RECORD_LENGTH_S = 60
   const val AUDIO_MIME_TYPE = MediaFormat.MIMETYPE_AUDIO_AAC
   const val RECORDED_VIDEO_CONTENT_TYPE: String = "video/mp4"
 
-  val DEFAULT_LVL1_STANDARD = QualityTier(resolution = 480, videoBitrateMbps = 1.0f, audioBitrateKbps = 128, maxDurationSec = 900)
-  val DEFAULT_LVL2_SHORT_STANDARD = QualityTier(resolution = 720, videoBitrateMbps = 2.0f, audioBitrateKbps = 128, maxDurationSec = 600)
-  val DEFAULT_LVL2_LONG_STANDARD = QualityTier(resolution = 720, videoBitrateMbps = 1.5f, audioBitrateKbps = 128, maxDurationSec = 900)
+  val DEFAULT_LVL1_STANDARD = QualityTier(resolution = VIDEO_SHORT_EDGE_SD, videoBitrateMbps = 3.5f, audioBitrateKbps = 192, maxDurationSec = 900)
+  val DEFAULT_LVL2_SHORT_STANDARD = QualityTier(resolution = VIDEO_SHORT_EDGE_HD, videoBitrateMbps = 4.5f, audioBitrateKbps = 192, maxDurationSec = 600)
+  val DEFAULT_LVL2_LONG_STANDARD = QualityTier(resolution = VIDEO_SHORT_EDGE_HD, videoBitrateMbps = 4.5f, audioBitrateKbps = 192, maxDurationSec = 900)
 
   @JvmStatic
-  val DEFAULT_HIGH = QualityTier(resolution = 720, videoBitrateMbps = 4.0f, audioBitrateKbps = 128, maxDurationSec = 360)
+  val DEFAULT_HIGH = QualityTier(resolution = VIDEO_SHORT_EDGE_HD, videoBitrateMbps = 6.0f, audioBitrateKbps = 192, maxDurationSec = 360)
 
   val DEFAULT_TRANSCODING_CONFIG = listOf(
     TranscodeConfig(
