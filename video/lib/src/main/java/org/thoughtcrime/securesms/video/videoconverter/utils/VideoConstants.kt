ce7774e8f4c893802591ec5e632b65a52a9d0050
Abandoned Cart <twistedumbrella@gmail.com>
2022-10-28 18:30:23 -0400

    Reduce media size restrictions and compression

--- video/lib/src/main/java/org/thoughtcrime/securesms/video/videoconverter/utils/VideoConstants.kt
+++ video/lib/src/main/java/org/thoughtcrime/securesms/video/videoconverter/utils/VideoConstants.kt
@@ -7,13 +7,13 @@ package org.thoughtcrime.securesms.video.videoconverter.utils
 
 import android.media.MediaFormat
 object VideoConstants {
-  const val AUDIO_BITRATE = 128_000
-  const val VIDEO_BITRATE_L1 = 1_250_000
-  const val VIDEO_BITRATE_L2 = 1_250_000
-  const val VIDEO_BITRATE_L3 = 2_500_000
-  const val VIDEO_SHORT_EDGE_SD = 480
-  const val VIDEO_SHORT_EDGE_HD = 720
-  const val VIDEO_LONG_EDGE_HD = 1280
+  const val AUDIO_BITRATE = 192_000
+  const val VIDEO_BITRATE_L1 = 3_500_000
+  const val VIDEO_BITRATE_L2 = 4_500_000
+  const val VIDEO_BITRATE_L3 = 6_000_000
+  const val VIDEO_SHORT_EDGE_SD = 720
+  const val VIDEO_SHORT_EDGE_HD = 1080
+  const val VIDEO_LONG_EDGE_HD = 1920
   const val VIDEO_MAX_RECORD_LENGTH_S = 60
   const val MAX_ALLOWED_BYTES_PER_SECOND = VIDEO_BITRATE_L3 / 8 + AUDIO_BITRATE / 8
   const val VIDEO_MIME_TYPE = MediaFormat.MIMETYPE_VIDEO_AVC
