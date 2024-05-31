3839c12034af26976fbef0d1b2e75c486c481f79
TwistedUmbrellaX <twistedumbrella@gmail.com>
2024-05-30 20:50:41 -0400

    Use build publishing time over creation times

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -624,6 +624,7 @@ dependencies {
 
   "playImplementation"(project(":billing"))
   "nightlyImplementation"(project(":billing"))
+  "bitImplementation"(project(":billing"))
 
   "spinnerImplementation"(project(":spinner"))
 
