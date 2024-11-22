7279368682a4a4e999717b34a80cd4cb995574a1
Greyson Parrelli <greyson@signal.org>
2024-11-22 10:53:58 -0500

    Bump version to 7.27.0
    Update libphonenumber to 8.13.50
    
    Import correct InvalidKeyException in LinkDeviceRepository.
    
    Resolves #13808
    
    Use the correct link device flag.
    
    Add support for the new Benin phone format.
    
    Update translations and other static files.
    
    Bump version to 7.26.1
    
    Update backup integration tests.
    
    Add new rules to log scrubber.
    
    Potential fix for a backup validation error.
    
    Force relays only if remote user is not a 'Signal Connection'.
    
    Add "your media will be deleted today" mechanism based off last checkin time and media TTL.
    
    Remove orphaned attachments when creating a new backup.
    
    Add sgnl://ideal support to app deeplinks.
    
    Add unit test for RegistrationUtil.
    
    Use AEP for regv3 flows.
    
    Fix creating message_fts table during database migration.
    
    Fixes #13034
    Fixes #13506
    
    Rebuild FTS index in V239_MessageFullTextSearchEmojiSupport.
    
    Rebuild FTS index in V242_MessageFullTextSearchEmojiSupportV2.
    
    Resolves #13810
    
    Fix multiple activity finish after donation.
    
    Fix icon gradient.
    
    Use ChatListener to get connection interrupted event from libsignal; clear connection state when received
    
    Flesh out restore paths for regv3.
    
    Update to libsignal 0.63.0
    
    Add an internal sqlite playground.
    
    Update linked devices strings.
    
    Allow renaming of linked device.
    
    Fix export bugs causing validation errors.
    
    Update GV2 spinner transformer to for group updates in message extras.
    
    Improve storage service diff logging.
    
    Remove some unnecessary call tab requeries.
    
    Verify group ids on peer-to-peer group changes.
    
    Inject SignalWebSocket into IncomingMessageObserver.
    
    Resolves #13820
    
    Use calling service type for calls.
    
    Kotlin-ize some more tests.
    
    Resolves #13813
    
    Fix empty username backupv2 export.
    
    Fix lost voice notes on orientation change.
    
    Allow group removal shortcut in chat folders.
    
    Use EmojiTextView for chat folder names.
    
    Center call link button.
    
    Add validation error UI.
    
    Split remote user check for group vs individual calls.
    
    Ensure updateMessageRequestAcceptedState runs on main thread.
    
    Convert more tests to kotlin.
    
    Resolves #13825
    
    Update most of the backup integration tests.
    
    Add some more link+sync logs.
    
    Use AEP when validating backup.
    
    Fix incorrect ACI/PNI storage for revoked invites in group updates.
    
    Fix denial dialog for call links.
    
    Handle web socket closed unexpectedly errors more gracefully.
    
    Inline the expireVersion capability.
    
    Inline the deleteSync capability.
    
    Fix calls silent ringer bug.
    
    Fix GSE crash when attempting to send to non-GV2 groups.
    
    Update to the latest backup tests.
    
    Add call link strings.
    
    Add sms provider failed specific messaging during registration.
    
    Better progress messaging.
    
    Update translations and other static files.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ plugins {
 
 apply(from = "static-ips.gradle.kts")
 
-val canonicalVersionCode = 1488
-val canonicalVersionName = "7.26.0"
+val canonicalVersionCode = 1490
+val canonicalVersionName = "7.27.0"
 val currentHotfixVersion = 0
 val maxHotfixVersions = 100
 
