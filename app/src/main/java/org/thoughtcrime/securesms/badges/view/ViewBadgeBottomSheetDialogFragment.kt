621503dc0cafeac2485c133243f86ff272d79075
Abandoned Cart <twistedumbrella@gmail.com>
2026-01-28 21:58:54 -0500

    Fix imports for conversion to Kotlin source

--- app/src/main/java/org/thoughtcrime/securesms/badges/view/ViewBadgeBottomSheetDialogFragment.kt
+++ app/src/main/java/org/thoughtcrime/securesms/badges/view/ViewBadgeBottomSheetDialogFragment.kt
@@ -14,6 +14,7 @@ import com.google.android.material.tabs.TabLayoutMediator
 import org.signal.core.ui.BottomSheetUtil
 import org.signal.core.ui.FixedRoundedCornerBottomSheetDialogFragment
 import org.signal.core.util.getParcelableCompat
+import org.thoughtcrime.securesms.BuildConfig
 import org.thoughtcrime.securesms.R
 import org.thoughtcrime.securesms.badges.BadgeRepository
 import org.thoughtcrime.securesms.badges.models.Badge
@@ -28,6 +29,7 @@ import org.thoughtcrime.securesms.util.CommunicationActions
 import org.thoughtcrime.securesms.util.ViewUtil
 import org.thoughtcrime.securesms.util.adapter.mapping.MappingAdapter
 import org.thoughtcrime.securesms.util.visible
+import java.util.Objects
 import kotlin.math.ceil
 import kotlin.math.max
 
